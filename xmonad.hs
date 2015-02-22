9976a171f837ba57854a9254843f60a7a0cd77e4
Luis <luis@decomputed.com>
2015-02-22 19:36:07 +0100

    Removes extra keys

--- xmonad.hs
+++ xmonad.hs
@@ -1,19 +1,16 @@
- import XMonad
- import XMonad.Hooks.ManageDocks
- import XMonad.Hooks.DynamicLog
- import XMonad.Util.Run
- import XMonad.Util.EZConfig (additionalKeys)
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run
+import XMonad.Util.EZConfig (additionalKeys)
 
- main = do
-   xmproc <- spawnPipe "/usr/bin/xmobar /home/luis/.xmobarrc"
-   xmonad $ defaultConfig {
-   manageHook = manageDocks <+> manageHook defaultConfig,
-   layoutHook = avoidStruts  $  layoutHook defaultConfig,
-   logHook = dynamicLogWithPP xmobarPP
-{ 
-  ppOutput = hPutStrLn xmproc,
-  ppTitle = xmobarColor "green" "" . shorten 50
-  }
-   }  `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        ]
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/luis/.xmobarrc"
+  xmonad $ defaultConfig {
+    manageHook = manageDocks <+> manageHook defaultConfig,
+    layoutHook = avoidStruts  $  layoutHook defaultConfig,
+    logHook = dynamicLogWithPP xmobarPP { 
+      ppOutput = hPutStrLn xmproc,
+      ppTitle = xmobarColor "green" "" . shorten 50
+      }
+    }
