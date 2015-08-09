8d4344640ee33b2cf204c308d3373bc64943b202
luis <luis@decomputed.com>
2015-08-09 11:12:55 +0200

    Changes needed for laptop

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 # Remember my language!
-set -x LANG en_GB.utf8
-set -x LC_ALL en_GB.utf8
+set -x LANG en_US.utf8
+set -x LC_ALL en_US.utf8
 set -x fish_color_cwd cyan
 
 # fish git prompt
@@ -44,17 +44,17 @@ function fish_prompt
 end
 
 # start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx
-    end
-end
+#if status --is-login
+#    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+#        exec startx
+#   end
+#end
 
 # Path updates
 set -x PATH $PATH ~/.cabal/bin /usr/bin ~/.bin
 
 # Java
-set -x JAVA_HOME /usr/lib/jvm/default
-set -x PATH $PATH $JAVA_HOME/bin
-set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-set -x _JAVA_AWT_WM_NONREPARENTING 1
+#set -x JAVA_HOME /usr/lib/jvm/default
+#set -x PATH $PATH $JAVA_HOME/bin
+#set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+#set -x _JAVA_AWT_WM_NONREPARENTING 1
