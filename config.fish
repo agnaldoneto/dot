7d69e7e04e7196838305ddafa96119d4213648ec
luis <luis@decomputed.com>
2015-09-28 10:23:35 +0200

    Adds ssh-agent configuration to config.fish

--- config.fish
+++ config.fish
@@ -1,16 +1,15 @@
-# Remember my language!
+# Language -- should still change this to en-GB
 set -x LANG en_US.utf8
 set -x LC_ALL en_US.utf8
+
+# CWD color
 set -x fish_color_cwd cyan
 
-# Splunkp
+# Splunk
 set -x SPLUNK_HOME /opt/splunk
 
 # Path updates
 set -x PATH $PATH ~/.cabal/bin /usr/bin ~/.bin $SPLUNK_HOME
 
-# Java
-#set -x JAVA_HOME /usr/lib/jvm/default
-#set -x PATH $PATH $JAVA_HOME/bin
-#set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-#set -x _JAVA_AWT_WM_NONREPARENTING 1
+# SSH Agent
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
